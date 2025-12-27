729b136b9edfeefc5a3b0fe954ecd32fe61dabd0
elisate <dushimiyimanaelisa@gmail.com>
2025-12-27 14:53:28 +0200

    fix version documentation

--- auth_service/src/app.module.ts
+++ auth_service/src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+    imports: [
+      ConfigModule.forRoot({ isGlobal: true }), // Load .env globally
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          type: 'postgres',
+          url: configService.get<string>('DATABASE_URL'), // ✅ use injected instance
+          // entities: [Product],
+          synchronize: true, // dev only
+          autoLoadEntities: true,
+        }), 
+      }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
